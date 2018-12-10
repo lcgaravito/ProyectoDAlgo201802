445d630bec70401794ae560b138e0c3c29a00f25
LUIS CARLOS GARAVITO ROMERO <lc.garavito@sis.virtual.uniandes.edu.co>
2018-12-10 13:38:20 -0500

    ProblemaB resuelto

--- src/ProblemaB.java
+++ src/ProblemaB.java
@@ -1,71 +1,86 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Programa para calcular el máximo capital que se puede esperar en el tiempo n según lo estipulado para el Problema B.
+ * @author Luis Carlos Garavito Romero. 201614451.<br>
+ * Miguel Ángel Puentes Ramírez. 201616771.
+ */
 public class ProblemaB {
 
+	/**
+	 * Ejecuta el programa.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		int n = 40;
-		int C = 100;
-		double rA[] = {-6, 5, 7, -5, -1, 2, 4, 9, 7, 5, 6, 3, 2, -4, 5, 2, 8, -5, 9, 7, 
-				-6, 5, 7, -5, -1, 2, 4, 9, 7, 5, 6, 3, 2, -4, 5, 2, 8, -5, 9, 7, 
-				8, 7, -4, 5, 2, 0, 1, 6, 9, -8, 7, -5, 3, 6, -5, 7, -2, 9, -5, 7, 
-				-6, 5, 7, -5, -1, 2, 4, 9, 7, 5, 6, 3, 2, -4, 5, 2, 8, -5, 9, 7, 
-				8, 7, -4, 5, 2, 0, 1, 6, 9, -8, 7, -5, 3, 6, -5, 7, -2, 9, -5, 7
-		};
-		double rB[] = {8, 7, -4, 5, 2, 0, 1, 6, 9, -8, 7, -5, 3, 6, -5, 7, -2, 9, -5, 7, 
-				-6, 5, 7, -5, -1, 2, 4, 9, 7, 5, 6, 3, 2, -4, 5, 2, 8, -5, 9, 7, 
-				8, 7, -4, 5, 2, 0, 1, 6, 9, -8, 7, -5, 3, 6, -5, 7, -2, 9, -5, 7, 
-				-6, 5, 7, -5, -1, 2, 4, 9, 7, 5, 6, 3, 2, -4, 5, 2, 8, -5, 9, 7, 
-				-6, 5, 7, -5, -1, 2, 4, 9, 7, 5, 6, 3, 2, -4, 5, 2, 8, -5, 9, 7
-		};
-		
-		long start = System.currentTimeMillis();
-		int answer = maximoCapital(n, C, rA, rB, false);
-		long end = System.currentTimeMillis();
-		System.out.println(answer);
-		System.out.println("Tiempo transcurrido: " + (end - start) + "ms");
+		int n;
+		int C;
+		double rA[];
+		double rB[];
+		ArrayList<Integer> answers = new ArrayList<Integer>();
+		InputStreamReader is = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(is);
+		ProblemaB instancia = new ProblemaB();
+		try
+		{
+			String line = br.readLine();
+			while(line!=null && line.length()>0 && !line.equals("0 0"))
+			{
+				n=Integer.parseInt(line.split(" ")[0]);
+				C=Integer.parseInt(line.split(" ")[1]);
+				String[] rAS = br.readLine().split(" ");
+				rA = new double[rAS.length];
+				for (int i = 0; i < rAS.length; i++) {
+					rA[i] = Double.parseDouble(rAS[i]);
+				}
+				String[] rBS = br.readLine().split(" ");
+				rB = new double[rBS.length];
+				for (int i = 0; i < rBS.length; i++) {
+					rB[i] = Double.parseDouble(rBS[i]);
+				}
+				int answer = instancia.maximoCapital(n, C, rA, rB, false);
+				answers.add(answer);
+				line = br.readLine().trim();
+			}
+			for (Integer integer : answers) {
+				System.out.println(integer);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			System.out.println("ERROR: Entrada con formato incorrecto.");
+		}
 	}
 
-	public static int maximoCapital(int n, int C, double[] rA, double[] rB, boolean bagA)
+	/**
+	 * Encuentra el máximo capital que se puede esperar en el tiempo n<br>
+	 * @param n Número de periodos en los que se desea invertir. n > 0.
+	 * @param C Capital inicial que se va a invertir. C > 0.
+	 * @param rA Arreglo con la rentabilidad de la bolsa A para cada periodo. rA.length = n.
+	 * @param rB Arreglo con la rentabilidad de la bolsa B para cada periodo. rB.length = n.
+	 * @param bagA Booleano para controlar las inversiones en la bolsa A.<br>
+	 * bagA es igual a true si en el periodo anterior se invirtió en la bolsa A y es igual a false de lo contrario.
+	 * @return Máximo capital que se puede esperar en el tiempo n.
+	 */
+	public int maximoCapital(int n, int C, double[] rA, double[] rB, boolean bagA)
 	{
-		System.out.println("n: " + n);
-		System.out.println(bagA);
 		double answer = 0;
-		if(n==1)
+		if(n==1)		// Caso base del algoritmo de programación dinámica.
 		{
-			if(!bagA)
-				answer = Double.max(C*(1+rA[n-1]/100), C*(1+rB[n-1]/100));
-			else
+			if(bagA)	// Si en el periodo anterior se invirtió en la bolsa A.
 				answer = C*(1+rB[n-1]/100);
+			else
+				answer = Double.max(C*(1+rA[n-1]/100), C*(1+rB[n-1]/100));
 		}
 		else
 		{
-			if(bagA)
+			if(bagA)	// Si en el periodo anterior se invirtió en la bolsa A.
 				answer = maximoCapital(n-1, (int) (C*(1+rB[n-1]/100)), rA, rB, false);
 			else
 			{
 				answer = Double.max(maximoCapital(n-1, (int) (C*(1+rB[n-1]/100)), rA, rB, false), maximoCapital(n-1, (int) (C*(1+rA[n-1]/100)), rA, rB, true));
-			}
-		}
-		return (int)answer;
-	}
-	
-	public static int maximoCapital2(int n, int C, double[] rA, double[] rB, boolean bagA)
-	{
-		System.out.println("n: " + n);
-		System.out.println(bagA);
-		double answer = 0;
-		if(n==1)
-		{
-			if(!bagA)
-				answer = Double.max(C*(1+rA[n-1]/100), C*(1+rB[n-1]/100));
-			else
-				answer = C*(1+rB[n-1]/100);
-		}
-		else
-		{
-			if(bagA)
-				answer = maximoCapital2(n-1, (int) (C*(1+rB[n-1]/100)), rA, rB, false);
-			else
-			{
-				answer = Double.max(maximoCapital2(n-1, (int) (C*(1+rB[n-1]/100)), rA, rB, false), maximoCapital2(n-1, (int) (C*(1+rA[n-1]/100)), rA, rB, true));
 			}
 		}
 		return (int)answer;
